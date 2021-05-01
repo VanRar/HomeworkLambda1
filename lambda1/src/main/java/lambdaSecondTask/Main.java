634839f468372162a220e416df66ab1565f7d38d
Vitaliy Fedyushev <vitalikf7@gmail.com>
2021-05-01 15:47:10 +0300

    написал первую часть второго ДЗ

---
+++ lambda1/src/main/java/lambdaSecondTask/Main.java
@@ -0,0 +1,11 @@
+package lambdaSecondTask;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker worker = new Worker(listener);
+        worker.start();
+
+    }
+}
